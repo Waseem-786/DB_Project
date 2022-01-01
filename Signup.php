f68372b22c9726c49d57f2e9f859fd0f15468e7a
Waseem Shahzad <w4w.w4me@gmail.com>
2022-01-01 20:58:13 +0500

    change

--- Signup.php
+++ Signup.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SignUp</title>
     <link rel="stylesheet" href="Signup.css">
-    <!-- <link rel="stylesheet" href="Signup.php"> -->
 </head>
 <body>
     <div class="container">
